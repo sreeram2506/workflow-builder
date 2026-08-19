315073f817220807f7351d421f86756b8aa7755e
sreeram2506 <sreeramchenchukumar@gmail.com>
2026-08-19 09:04:08 +0530

    removed tryroute

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { ShellLayoutComponent } from './features/shell/shell-layout.component';
 import { AgentSkillsShellComponent } from './features/agent/agent-skills-shell.component';
-import { TryUiHostComponent } from './try/try-ui-host.component';
 
 export const routes: Routes = [
   { path: '', component: ShellLayoutComponent },
-  { path: 'try-ui', component: TryUiHostComponent }, // this is for testing 
   { path: 'agent/:nodeId', component: AgentSkillsShellComponent },
   { path: '**', redirectTo: '' },
 ];
